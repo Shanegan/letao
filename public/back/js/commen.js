f15e28c6bc157b8d161f25b5b74310573b33d405
shaneGan <165337329@qq.com>
2018-01-12 14:30:51 +0800

    首页侧边栏静态完成

--- public/back/js/commen.js
+++ public/back/js/commen.js
@@ -1,3 +1,39 @@
 /**
  * Created by Administrator on 2018/1/11 0011.
  */
+$(function () {
+  console.log(NProgress);
+  NProgress.configure({
+    showSpinner: false//关闭进度环
+  });
+  $(document).ajaxStart(function () {
+    NProgress.start();
+  });
+
+  $(document).ajaxStop(function () {
+    setTimeout(function () {
+      NProgress.done();
+    }, 1000);
+  });
+
+
+  //在飞登录页面，发送ajax请求，询问用户是否登录，如果没有登录。。。。
+  //if (location.href.indexOf("login.html") == -1) {
+  //  $.ajax({
+  //    type:"get",
+  //    url:"/employee/checkRootLogin",
+  //    success:function(info){
+  //      if(info.error == 400){
+  //        locatin.href = "login.html";
+  //      }
+  //    }
+  //  });
+  //}
+
+
+  //二级菜单显示隐藏功能
+  $(".second").prev().on("click",function(){
+    $(this).next().slideToggle();
+  })
+
+})
